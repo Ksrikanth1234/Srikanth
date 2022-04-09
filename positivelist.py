3a3d00042997580d8678a12fea6d5fc47e95291f
Ksrikanth1234 <103011616+Ksrikanth1234@users.noreply.github.com>
2022-04-09 19:55:43 +0530

    positivelist.py
    this program prints positive values

---
+++ positivelist.py
@@ -0,0 +1,22 @@
+#first program
+a=[1,-2,4,-4]
+b=[]
+for i in a:
+    if i>0:
+        b.append(i)
+
+
+print("output:",b)
+
+
+#second program
+
+list1=[24,-42,94,-56,90]
+list2=[]
+for i in list1:
+    if i>0:
+        list2.append(i)
+
+
+
+print("output:",list2)        
